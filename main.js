4884c65f0245fb82261ec3f95a512ca76b77c71e
Eryofus <50362772+Eryofus@users.noreply.github.com>
2019-05-24 12:40:34 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,162 @@
+var apps = require("./apps.json");
+var request = require('request-promise-native');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+const { DownloaderHelper } = require('node-downloader-helper');
+const filenamify = require('filenamify');
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: { h: 'help', t: "threads", a: "apk,", p: "premium", f: "fresh" },
+    default: { t: '5' },
+});
+
+const getApps = async () => {
+    try {
+        return await request.get('https://devs.ouya.tv/api/v1/apps');
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApkLink(app) {
+    return new Promise(async (resolve, reject) => {
+
+        let downloadLink
+
+        try {
+            downloadLink = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid + "/download");
+
+            downloadLink = JSON.parse(downloadLink).app.downloadLink
+            uri = url.parse(downloadLink)
+            filename = path.basename(uri.path)
+
+            resolve([downloadLink, filename])
+
+        } catch (error) {
+            console.error("Error downloading the " + app.title + " APK")
+            reject()
+        }
+    })
+}
+
+async function downloadScreenshots(app) {
+
+    return new Promise(async resolve => {
+        let appJSON
+
+        try {
+            appJSON = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid);
+        } catch (error) {
+            console.error(error)
+        }
+
+        let screenshots = JSON.parse(appJSON).app.filepickerScreenshots
+
+        for (scr in screenshots) {
+            download(screenshots[scr], "./downloads/" + app.title + "/screenshots/", "screenshot" + scr + ".jpg")
+        }
+
+    })
+}
+
+async function downloadAsync(link, path, filename) {
+    return new Promise(async resolve => {
+
+        await download(link, path, filename).then(resolve())
+
+    })
+}
+
+async function download(link, path, filename) {
+    try {
+        return new Promise(async resolve => {
+
+            try {
+
+                await fs.mkdirSync(path, { recursive: true }, (err) => {
+                    if (err) throw err;
+                });
+
+                const dl = new DownloaderHelper(link, path, { fileName: filename, override: true });
+
+                dl.on('end', () => {
+                    resolve();
+                })
+
+                dl.on('error', (err) => {
+                    //dl.stop();
+                    //console.log(err)
+                    //console.log("Error")
+                    //console.log(link)
+                })
+
+                await dl.start();
+
+            } catch (error) {
+
+                console.error("Error downloading " + link)
+
+            }
+        })
+    } catch{ }
+}
+
+
+async function main() {
+
+    if (argv.f) apps = await JSON.parse(await getApps());
+
+    running = 0
+    limit = argv.t
+    x = 0
+
+    while (x < apps.apps.length) {
+        if (running < limit) {
+            app = apps.apps[x]
+            x++;
+            if (app.premium == false || argv.p) {
+                running++;
+                console.log("Downloading: " + app.title);
+                try {
+                    const appTitle = await filenamify(app.title, { replacement: '_' });
+                    if (!argv.a) {
+                        download("https://devs.ouya.tv/api/v1/apps/" + app.uuid, "./downloads/" + appTitle, "info.json")
+                        if (app.mainImageFullUrl != null) download(app.mainImageFullUrl, "./downloads/" + appTitle, "mainImage.png")
+                        if (app.heroImage != null) download(app.heroImage, "./downloads/" + appTitle, "heroImage.jpg")
+                        downloadScreenshots(app)
+                    }
+
+                    if (app.premium == false) {
+                        try {
+                            var [downloadLink, filename] = await getApkLink(app);
+                            download(downloadLink, "./downloads/" + appTitle + "/", filename).then(() => {
+                                running = running - 1;
+                            })
+                        } catch{ }
+                    }
+                } catch (error) { }
+            }
+        } else {
+            await sleep(100);
+        }
+    }
+}
+
+if (argv.h) {
+    console.log(`
+    OuyaArchiver v1
+
+    Options:
+        -t, --threads      set number of threads (default: 5)
+        -a, --apk          only download the APKs
+        -f, --fresh        use app list from the web, instead of the included one (adds a delay when starting)
+        -p, --premium      additionally downloads the info and screenshots of premium games, but not the APKs
+        -h, --help         print usage information
+    `)
+} else {
+    main();
+}
