e3f65efb8a9dd676c6173170bb409e50ec587af6
Eryofus <50362772+Eryofus@users.noreply.github.com>
2019-05-25 12:41:36 +0200

    General stability improvements to enhance the user's experience.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var request = require('request-promise-native');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+const asyn = require('async');
 const { DownloaderHelper } = require('node-downloader-helper');
 const filenamify = require('filenamify');
 var argv = require('minimist')(process.argv.slice(2), {
@@ -18,7 +19,7 @@ const getApps = async () => {
     try {
         return await request.get('https://devs.ouya.tv/api/v1/apps');
     } catch (error) {
-        console.error(error)
+        throw error
     }
 }
 
@@ -27,130 +28,107 @@ function sleep(ms) {
 }
 
 async function getApkLink(app) {
-    return new Promise(async (resolve, reject) => {
+    let downloadLink
 
-        let downloadLink
+    try {
+        downloadLink = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid + "/download");
 
-        try {
-            downloadLink = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid + "/download");
+        downloadLink = JSON.parse(downloadLink).app.downloadLink
+        const uri = url.parse(downloadLink)
+        const filename = path.basename(uri.path)
 
-            downloadLink = JSON.parse(downloadLink).app.downloadLink
-            uri = url.parse(downloadLink)
-            filename = path.basename(uri.path)
+        return [downloadLink, filename];
 
-            resolve([downloadLink, filename])
-
-        } catch (error) {
-            console.error("Error downloading the " + app.title + " APK")
-            reject()
-        }
-    })
+    } catch (error) {
+        console.error("Error downloading the " + app.title + " APK")
+        throw error
+    }
 }
 
 async function downloadScreenshots(app) {
+    let appJSON
 
-    return new Promise(async resolve => {
-        let appJSON
+    try {
+        appJSON = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid);
+    } catch (error) {
+        throw error
+    }
+
+    let screenshots = JSON.parse(appJSON).app.filepickerScreenshots
+
+    for (scr in screenshots) {
+        await download(screenshots[scr], "./downloads/" + app.title + "/screenshots/", "screenshot" + scr + ".jpg")
+    }
+
+}
+
+function download(link, path, filename) {
+    return new Promise(async (resolve, reject) => {
 
         try {
-            appJSON = await request.get("https://devs.ouya.tv/api/v1/apps/" + app.uuid);
+
+            await fs.mkdirSync(path, { recursive: true }, (err) => {
+                if (err) throw err;
+            });
+
+            const dl = new DownloaderHelper(link, path, { fileName: filename, override: true });
+
+            dl.on('end', () => {
+                resolve();
+            })
+
+            dl.on('error', (err) => {
+            })
+
+            await dl.start();
+
         } catch (error) {
-            console.error(error)
+
+            console.error("Error downloading " + link)
+            reject(error);
+
         }
-
-        let screenshots = JSON.parse(appJSON).app.filepickerScreenshots
-
-        for (scr in screenshots) {
-            download(screenshots[scr], "./downloads/" + app.title + "/screenshots/", "screenshot" + scr + ".jpg")
-        }
-
     })
 }
 
-async function downloadAsync(link, path, filename) {
-    return new Promise(async resolve => {
-
-        await download(link, path, filename).then(resolve())
-
-    })
-}
-
-async function download(link, path, filename) {
-    try {
-        return new Promise(async (resolve, reject) => {
-
-            try {
-
-                await fs.mkdirSync(path, { recursive: true }, (err) => {
-                    if (err) throw err;
-                });
-
-                const dl = new DownloaderHelper(link, path, { fileName: filename, override: true });
-
-                dl.on('end', () => {
-                    resolve();
-                })
-
-                dl.on('error', (err) => {
-                })
-
-                await dl.start();
-
-            } catch (error) {
-
-                console.error("Error downloading " + link)
-                reject();
-
-            }
-        })
-    } catch{ }
-}
 
 
 async function main() {
 
-    if (argv.f) apps = await JSON.parse(await getApps());
+    if (argv.f) apps = JSON.parse(await getApps());
 
-    running = 0
-    limit = argv.t
-    x = 0
+    const limit = argv.t;
 
-    while (x < apps.apps.length) {
-        if (running < limit) {
-            app = apps.apps[x]
-            x++;
-            if ((app.premium == false || argv.p) && !archive[app.uuid]) {
-                running++;
-                console.log("Downloading: " + app.title);
-                try {
-                    const appTitle = await filenamify(app.title, { replacement: '_' });
-                    if (!argv.a) {
-                        download("https://devs.ouya.tv/api/v1/apps/" + app.uuid, "./downloads/" + appTitle, "info.json")
-                        if (app.mainImageFullUrl != null) download(app.mainImageFullUrl, "./downloads/" + appTitle, "mainImage.png")
-                        if (app.heroImage != null) download(app.heroImage, "./downloads/" + appTitle, "heroImage.jpg")
-                        downloadScreenshots(app)
-                    }
+    asyn.eachOfLimit(apps.apps, limit, async (app, key, cb) => {
+        if ((app.premium == false || argv.p) && !archive[app.uuid]) {
+            console.log("Downloading: " + app.title + " (" + (key + 1) + "/" + apps.apps.length + ")");
+            try {
+                const appTitle = await filenamify(app.title, { replacement: '_' });
+                if (!argv.a) {
+                    await download("https://devs.ouya.tv/api/v1/apps/" + app.uuid, "./downloads/" + appTitle, "info.json")
+                    if (app.mainImageFullUrl != null) await download(app.mainImageFullUrl, "./downloads/" + appTitle, "mainImage.png")
+                    if (app.heroImage != null) await download(app.heroImage, "./downloads/" + appTitle, "heroImage.jpg")
+                    await downloadScreenshots(app)
+                }
 
-                    if (app.premium == false) {
-                        try {
-                            var [downloadLink, filename] = await getApkLink(app).then(new Error());
-                            download(downloadLink, "./downloads/" + appTitle + "/", filename).then(() => {
-                                running = running - 1;
-                                archive[app.uuid] = true;
-                                fs.writeFileSync("archive.json", JSON.stringify(archive));
-                            })
-                        } catch{ running = running - 1; }
-                    } else {
-                        running = running - 1;
+                if (app.premium == false) {
+                    var [downloadLink, filename] = await getApkLink(app).then(new Error());
+                    await download(downloadLink, "./downloads/" + appTitle + "/", filename).then(() => {
                         archive[app.uuid] = true;
                         fs.writeFileSync("archive.json", JSON.stringify(archive));
-                    }
-                } catch (error) { }
-            }
-        } else {
-            await sleep(100);
-        }
-    }
+                    })
+
+                } else {
+                    archive[app.uuid] = true;
+                    fs.writeFileSync("archive.json", JSON.stringify(archive));
+                }
+            } catch (error) { console.log(error) }
+        } else { }
+        if (cb) cb();
+    }, (err) => {
+        console.error(err)
+    })
+
 }
 
 if (argv.h) {
